52ca417b4cf7e745135f03a95614fd5c57c8b8df
Lukas Harbarth <lukas.harbarth@sap.com>
2025-03-13 10:59:39 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  BarcodeScannerDialog,
+  Button,
+  FlexBox,
+  ShellBar,
+} from "@ui5/webcomponents-react";
+function App() {
+  return (
+    <>
+      {/*  @ui5/webcomponents-fiori component*/}
+      <ShellBar primaryTitle="Shellbar" />
+      {/*  @ui5/webcomponents component*/}
+      <Button>Button</Button>
+      {/*  @ui5/webcomponents-react component*/}
+      <FlexBox>
+        <span>FlexBox</span>
+      </FlexBox>
+      {/*  zxing component*/}
+      <BarcodeScannerDialog />
+    </>
+  );
+}
+
+export default App;
